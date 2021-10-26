2ab8f4a37e534f8314a8724328876ad5e3a80dd1
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2021-10-26 19:39:21 +0100

    signup api setup

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,98 @@
 $(function() {
 
 
-    console.log($('#signup').validate())
+    $('#signup').validate({
+        rules: {
+            email: {
+                required: {
+                    depends: function() {
+                        $(this).val($.trim($(this).val()));
+                        return true;
+                    }
+                },
+                email: true,
+            },
+            firstname: {
+                required: {
+                    depends: function() {
+                        $(this).val($.trim($(this).val()));
+                        return true;
+                    }
+                },
+                lettersonly: true,
+            },
+            lastname: {
+                required: {
+                    depends: function() {
+                        $(this).val($.trim($(this).val()));
+                        return true;
+                    }
+                },
+                lettersonly: true,
+            },
+            phone: {
+                required: {
+                    depends: function() {
+                        $(this).val($.trim($(this).val()));
+                        return true;
+                    }
+                },
+                minlength: 7,
+                integer: true,
+            },
+            address: {
+                required: true
+            }
+        },
+        submitHandler: function(form) {
+            $(form).find('button').attr('disabled', true)
+            $(form).ajaxSubmit(options);
+        }
+    });
+
+    const options = {
+        type: 'POST',
+        url: "http://dregstage.herokuapp.com/api/v1/user/sign_up/",
+        data: $(this).serialize(),
+        dataType: 'json',
+        clearForm: null,
+        success: function(response) {
+            console.log(response)
+            // toastr.clear();
+            // toastr.options = {
+            //     "timeOut": "50000",
+            // }
+            // NioApp.Toast('User Created Successfully!', 'success', {position: 'top-left'});
+            // $('#users_table').Datatable().ajax().reload();
+            // setTimeout( () =>  window.location.replace(`${window.location.origin}${window.location.pathname}`), 3000);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest.status)
+            console.log(XMLHttpRequest.statusText)
+            console.log(textStatus)
+            console.log(errorThrown)
+
+            let errors = XMLHttpRequest.responseJSON;
+            console.log(errors)
+            // set up alert
+    
+            // $(user).find('button').attr('disabled', false);
+    
+            // // display toast alert
+            // toastr.clear();
+            // toastr.options = {
+            //     "timeOut": "7000",
+            // }
+            // NioApp.Toast('Unable to process request now.', 'error', {position: 'top-right'});
+        }
+    };
 
 
     // $('#signup').on('submit', function(e) {
     //     e.preventDefault();
 
 
-    //     $.post("http://dregstage.herokuapp.com/api/v1/user/sign_up/", 
+    //     $.post("", 
     //         {
     //             "email": "alabi@gmail.com",
     //             "firstname": "Emmanuel",
